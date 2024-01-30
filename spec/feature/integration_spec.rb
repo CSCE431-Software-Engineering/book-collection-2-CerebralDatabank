f236e7857d4073b99ca5966a7ddd4d5551a5ee7e
Gopal Othayoth <Gopal.Othayoth@outlook.com>
2024-01-29 22:01:57 -0600

    Fix integration tests again

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in "book[price]", with: '9.99'
     fill_in "book[pub_date]", with: '2024-01-29'
     click_on 'Create Book'
-    visit books_path
     expect(page).to have_content('Harry Potter')
     expect(page).to have_content('J.K. Rowling')
+    expect(page).to have_content('success') # flash success message
     click_on 'Show'
     expect(page).to have_content('9.99')
   end
@@ -21,8 +21,8 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in "book[price]", with: '9.99'
     fill_in "book[pub_date]", with: '2024-01-29'
     click_on 'Create Book'
-    visit books_path
     expect(page).not_to have_content('J.K. Rowling')
+    expect(page).to have_content('fail') # flash failure message
   end
   scenario 'invalid author' do
     visit new_book_path
@@ -31,18 +31,18 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in "book[price]", with: '9.99'
     fill_in "book[pub_date]", with: '2024-01-29'
     click_on 'Create Book'
-    visit books_path
     expect(page).not_to have_content('Harry Potter')
+    expect(page).to have_content('fail')
   end
   scenario 'invalid price' do
     visit new_book_path
     fill_in "book[title]", with: 'Harry Potter'
     fill_in "book[author]", with: 'J.K. Rowling'
-    fill_in "book[price]", with: '-1.00'
+    fill_in "book[price]", with: '' # CHANGE
     fill_in "book[pub_date]", with: '2024-01-29'
     click_on 'Create Book'
-    visit books_path
     expect(page).not_to have_content('Harry Potter')
+    expect(page).to have_content('fail')
   end
   scenario 'invalid date' do
     visit new_book_path
@@ -51,7 +51,7 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in "book[price]", with: '9.99'
     fill_in "book[pub_date]", with: '3070-01-01'
     click_on 'Create Book'
-    visit books_path
     expect(page).not_to have_content('Harry Potter')
+    expect(page).to have_content('fail')
   end
 end
