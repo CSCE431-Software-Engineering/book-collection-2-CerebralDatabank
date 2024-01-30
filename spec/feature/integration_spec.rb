2a82619b9c7685086c389fa3da8f6c42138a0b64
Gopal Othayoth <Gopal.Othayoth@outlook.com>
2024-01-29 19:52:32 -0600

    Add integration & unit tests for addl. attributes
    Additional attributes: author, price, and pub_date (published date)

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -3,15 +3,59 @@ require 'rails_helper'
 RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
-    fill_in "book[title]", with: 'harry potter'
+    fill_in "book[title]", with: 'Harry Potter'
+    fill_in "book[author]", with: 'J.K. Rowling'
+    fill_in "book[price]", with: '9.99'
+    fill_in "book[pub_date]", with: '2024-01-29'
     click_on 'Create Book'
     visit books_path
-    expect(page).to have_content('harry potter')
+    expect(page).to have_content('Harry Potter')
+    expect(page).to have_content('J.K. Rowling')
+    expect(page).to have_content('9.99')
+    expect(page).to have_content('success') # flash success message
   end
-  scenario 'invalid inputs' do
+  scenario 'invalid title' do
     visit new_book_path
     fill_in "book[title]", with: ''
+    fill_in "book[author]", with: 'J.K. Rowling'
+    fill_in "book[price]", with: '9.99'
+    fill_in "book[pub_date]", with: '2024-01-29'
     click_on 'Create Book'
-    expect(page).to have_content('fail') # flash alert message
+    expect(page).to have_content('fail') # flash failure message
+    visit books_path
+    expect(page).not_to have_content('J.K. Rowling')
+  end
+  scenario 'invalid author' do
+    visit new_book_path
+    fill_in "book[title]", with: 'Harry Potter'
+    fill_in "book[author]", with: ''
+    fill_in "book[price]", with: '9.99'
+    fill_in "book[pub_date]", with: '2024-01-29'
+    click_on 'Create Book'
+    expect(page).to have_content('fail')
+    visit books_path
+    expect(page).not_to have_content('Harry Potter')
+  end
+  scenario 'invalid price' do
+    visit new_book_path
+    fill_in "book[title]", with: 'Harry Potter'
+    fill_in "book[author]", with: 'J.K. Rowling'
+    fill_in "book[price]", with: '-1.00'
+    fill_in "book[pub_date]", with: '2024-01-29'
+    click_on 'Create Book'
+    expect(page).to have_content('fail')
+    visit books_path
+    expect(page).not_to have_content('Harry Potter')
+  end
+  scenario 'invalid date' do
+    visit new_book_path
+    fill_in "book[title]", with: 'Harry Potter'
+    fill_in "book[author]", with: 'J.K. Rowling'
+    fill_in "book[price]", with: '9.99'
+    fill_in "book[pub_date]", with: '3070-01-01'
+    click_on 'Create Book'
+    expect(page).to have_content('fail')
+    visit books_path
+    expect(page).not_to have_content('Harry Potter')
   end
 end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'Harry Potter', author: 'J.K. Rowling', price: 9.99, pub_date: '1997-06-26')
   end
 
   it 'is valid with valid attributes' do
@@ -12,6 +12,21 @@ RSpec.describe Book, type: :model do
 
   it 'is not valid without a name' do
     subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without an author' do
+    subject.auther = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a published date' do
+    subject.pub_date = nil
     expect(subject).not_to be_valid
   end
 end
